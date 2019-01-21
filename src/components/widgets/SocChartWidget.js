50956975c6449c656cc21fcdb735e07111ffca75
simon <simonbrunet@Simons-MacBook-Pro.local>
2019-01-21 19:23:08 +0100

    fix build issues

--- src/components/widgets/SocChartWidget.js
+++ src/components/widgets/SocChartWidget.js
@@ -30,21 +30,11 @@ class SocChartWidget extends React.Component {
   render() {
     console.log(this.state.socs);
 
-    const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
     return(
       <div className="row">
         <div className="col-lg-12">
           <div className="white-box">
-            <AreaChart width={600} height={400} data={this.state.soc} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            <AreaChart width={600} height={400} data={this.state.socs} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
               <CartesianGrid strokeDasharray="3 3"/>
               <XAxis dataKey="time"/>
               <YAxis/>
